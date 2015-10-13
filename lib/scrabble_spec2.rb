263eae13a9051f5b23701a39e2d5596b996a5341
Tammy <misstamar@gmail.com>
2015-10-13 16:23:14 -0700

    check

---
+++ lib/scrabble_spec2.rb
@@ -0,0 +1,17 @@
+require "./scrabble_class.rb"
+
+describe Scrabble do
+  before :each do
+    @word = Scrabble.new
+  end
+  describe  "word_value"
+   it "empty word do nothing"
+   expect (@word).to
+  describe "letter_value" do
+    it "return the value of the letter A" do
+      expect(A).to equal 1
+    end
+    it "return the value"
+
+  end
+end
