0150b6f1359b80319af23ec928f1c6a2dbc78e8c
Tammy <misstamar@gmail.com>
2015-10-13 16:53:50 -0700

    case method

--- spec/scrabble_spec.rb
+++ spec/scrabble_spec.rb
@@ -1,17 +1,29 @@
 require "./lib/scrabble_class.rb"
 
 describe Scrabble do
-  before :each do
-    @word = Scrabble.new
-  end
-  describe  "word_value"
-   it "empty word do nothing"
-   expect (@word).to
+
   describe "letter_value" do
-    it "return the value of the letter A" do
-      expect(A).to equal 1
+    it "return the value of the letter 'A'" do
+      expect letter_value("a").to equal 1
     end
-    it "return the value"
+    it "return the value of the letter 'D'" do
+      expect letter_value("d").to equal 2
+    end
+    it "return the value of the letter 'B'" do
+      expect letter_value("b").to equal 3
+    end
+    it "return the value of the letter 'F'" do
+      expect letter_value("f").to equal 4
+    end
+    it "return the value of the letter 'K'" do
+      expect letter_value("k").to equal 5
+    end
+    it "return the value of the letter 'J'" do
+      expect letter_value("j").to equal 8
+    end
+    it "return the value of the letter 'Q'" do
+      expect letter_value("q").to equal 10
+    end
 
   end
 end
