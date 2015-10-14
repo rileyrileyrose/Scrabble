8bd0119ebafff438daf7765f3ab58ab1974dddd3
Tammy <misstamar@gmail.com>
2015-10-14 09:17:23 -0700

    score nethod has been comlplited

--- lib/scrabble_class.rb
+++ lib/scrabble_class.rb
@@ -15,7 +15,7 @@ module Scrabble
       # if multiple words are same, choose first in list
     end
 
-    def letter_value(letter)
+    def self.letter_value(letter)
       case letter
         when "e", "a", "i", "o", "u", "l", "n", "r", "s", "t"
           return 1
@@ -32,7 +32,27 @@ module Scrabble
         when "q", "z"
           return 10
       end
+    end
 
+    #check that it's actually a letter - 0 for letter, nil if not
+    def letter?(lookAhead)
+        reutrn lookAhead =~ /[[:alpha:]]/
+    end
+
+    def self.score(word)
+      if word == nil
+        return nil
+      else
+        word_array = word_value.split("")
+        score = 0
+        word_array.each |n| do
+          if letter?(n) == 0 do
+            score += letter_value(n)
+          else
+            return nil # or an error ?
+          end
+        return score
+      end
     end
   end
 end
