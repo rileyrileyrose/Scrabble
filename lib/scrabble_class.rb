ac63527c0230214b758a81a98a24d32eeda8d4a9
Tammy <misstamar@gmail.com>
2015-10-14 10:08:18 -0700

    update 'letter value

--- lib/scrabble_class.rb
+++ lib/scrabble_class.rb
@@ -40,10 +40,11 @@ module Scrabble
             # 50 pt bonus for using all 7 tiles
             # if multiple words are same, choose first in list
     end
-
+    
+    # returns the total score value for the given word.
     def self.score(word)
       if word == nil
-        return nil
+        puts "You need to write a word" #double check
       else
         word_array = word_value.split("")
         score = 0
@@ -51,7 +52,8 @@ module Scrabble
           if letter?(n) == 0 do
             score += letter_value(n)
           else
-            return nil # or an error ?
+            raise ArguemetError.new("You need to write only letters")
+            break
           end
         return score
       end
