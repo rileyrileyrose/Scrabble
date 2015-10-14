17adc3b40620609596b8e3a3469d9746276ce0f3
Riley Spicer <riley.r.spicer@gmail.com>
2015-10-14 14:38:53 -0700

    Getting rid of hashes

--- lib/scrabble_class.rb
+++ lib/scrabble_class.rb
@@ -31,8 +31,6 @@ module Scrabble
 
 
     def self.highest_score_from(array_of_words)
-      # returns the word in an array with the highest score
-      scores_hash = {}
       #Make sure every word is a string
       array_of_words = array_of_words.each {|word| word = word.to_s}
       # Makes sure no elements of the array are nil
@@ -40,22 +38,11 @@ module Scrabble
         raise ArgumentError, "This array contains a nil value."
       end
       # Find score of each word, put into hash
-      array_of_words.each do |word|
-        scores_hash[Scrabble.score(word)] = word
-      end
-      # Sort through scores_hash and if value is a string with 7 letters, add 50 to the score (the key of the key/value pair)
-      # Find high scores
-      @high_scores = scores_hash.select{|s, w| s == scores_hash.keys.max}
-
-      # What to do with ties
-      if @high_scores.length != 1
-
-        return @high_scores
-      end
+    
       # better to use fewer tiles
       # 50 pt bonus for using all 7 tiles
       # if multiple words are same, choose first in list
-      return @high_scores[scores_hash.keys.max]
+
     end
 
     #returns the total score value for the given word.
