5716a3e4fa0d80d79a731bbbae001c8b55fc112b
Tammy <misstamar@gmail.com>
2015-10-15 16:50:10 -0700

    initialize method looks good

--- lib/tile_bag.rb
+++ lib/tile_bag.rb
@@ -1,12 +1,43 @@
 module Scrabble
 
   class TileBag
+    attr_accessor :tile_bag
 
     def initialize
-      @tiles
-
+      @tile_bag = creat_tile_bag
     end
 
+    def creat_tile_bag
+      array_tile_bag = []
+      9.times { array_tile_bag.push("a")}
+      2.times { array_tile_bag.push("b")}
+      2.times { array_tile_bag.push("c")}
+      4.times { array_tile_bag.push("d")}
+      12.times { array_tile_bag.push("e")}
+      2.times { array_tile_bag.push("f")}
+      3.times { array_tile_bag.push("g")}
+      2.times { array_tile_bag.push("h")}
+      9.times { array_tile_bag.push("i")}
+      1.times { array_tile_bag.push("j")}
+      1.times { array_tile_bag.push("k")}
+      4.times { array_tile_bag.push("l")}
+      2.times { array_tile_bag.push("m")}
+      6.times { array_tile_bag.push("n")}
+      8.times { array_tile_bag.push("o")}
+      2.times { array_tile_bag.push("p")}
+      1.times { array_tile_bag.push("q")}
+      6.times { array_tile_bag.push("r")}
+      4.times { array_tile_bag.push("s")}
+      6.times { array_tile_bag.push("t")}
+      4.times { array_tile_bag.push("u")}
+      2.times { array_tile_bag.push("v")}
+      2.times { array_tile_bag.push("w")}
+      1.times { array_tile_bag.push("x")}
+      2.times { array_tile_bag.push("y")}
+      1.times { array_tile_bag.push("z")}
+      return array_tile_bag
+    end
+    
     def draw_tiles(num)
 
     end
--- spec/tile_bag_spec.rb
+++ spec/tile_bag_spec.rb
@@ -1,13 +1,16 @@
 require "./lib/tile_bag.rb"
 
 describe Scrabble::TileBag do
-
-  describe "Tile Bag" do
-
-    describe 'initialize' do
-
-    end
-
+  before :each do
+    @tile_bag =Scrabble::TileBag.new
   end
 
+  describe 'initialize' do
+    it "make a new tile bag" do
+      expect(@tile_bag.tile_bag).to be_an(Array)
+    end
+    it "last letter in the array is Z" do
+      expect(@tile_bag.tile_bag.last).to eq("z")
+    end
+  end
 end
