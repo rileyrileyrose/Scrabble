51429ecd880a3dabc3e3b6f16dd05c332459d5d4
Riley Spicer <riley.r.spicer@gmail.com>
2015-10-15 14:41:20 -0700

    Minor changes

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -5,20 +5,20 @@ describe Scrabble::Player do
     @player = Scrabble::Player.new("Ada")
   end
 
-  describe "initialize" do
+  describe "#initialize" do
   it "has a name" do
     expect(@player.name).to eq("Ada")
     end
   end
 
-  describe "total_score" do
+  describe "#total_score" do
     it  "sums up and returns scores" do
       @player.plays = ["aaa", "b", "c"]
       @player.total_score
       expect(@player.score).to eq(9)
     end
 
-    describe "won?" do
+    describe "#won?" do
       context "player has > 100 points" do
         @player.score = 300
         expect{@player.won?}.to eq(true)
@@ -28,6 +28,8 @@ describe Scrabble::Player do
         expect{@player.won?}.to eq(false)
       end
     end
+
+    describe "#highest_scoring_word"
 
   end
 
