a2c178d72e48ca5b61ed16dd5ba26042cd730624
Riley Spicer <riley.r.spicer@gmail.com>
2015-10-15 16:26:02 -0700

    spec for draw_tiles written and failed

--- spec/tile_bag_spec.rb
+++ spec/tile_bag_spec.rb
@@ -3,11 +3,24 @@ require "./lib/tile_bag.rb"
 describe Scrabble::TileBag do
 
   describe "Tile Bag" do
+    before :each do
+      @tile_bag = Scrabble::TileBag.new
+    end
 
     describe 'initialize' do
 
     end
 
-  end
+    describe '#draw_tiles(num)' do
+      it 'returns num tiles' do
+        tiles = @tile_bag.draw_tiles(4)
+        expect(tiles.length).to eq(4)
+      end
+      it 'removes the tiles from tile_bag_array' do
 
+      end
+    end
+
+
+  end
 end
