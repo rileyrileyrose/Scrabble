b9a9e793a1b8feb4ab6bdb61788ec4f49c28bc60
Riley Spicer <riley.r.spicer@gmail.com>
2015-10-16 11:06:57 -0700

    Fixed broken tilebag lib and specs

--- spec/tile_bag_spec.rb
+++ spec/tile_bag_spec.rb
@@ -23,13 +23,10 @@ describe Scrabble::TileBag do
      expect(tiles.length).to eq(4)
    end
    it 'removes the tiles from tile_bag_array' do
-     initial_length = @tile_bag.length
+     initial_length = @tile_bag.tile_bag.length
      @tile_bag.draw_tiles(1)
-     expect(@tile_bag.length).to eq(initial_length - 1)
+     expect(@tile_bag.tile_bag.length).to eq(initial_length - 1)
    end
   end
-<<<<<<< HEAD
 
-=======
->>>>>>> 458e4d9fe1029ff2b4edf6773095e9cb54ba17c0
 end
