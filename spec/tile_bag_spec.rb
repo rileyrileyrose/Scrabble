c7e7d182e421d6aa823429d721f4caa7972d698f
Tammy <misstamar@gmail.com>
2015-10-16 09:44:31 -0700

    commit to merge

--- spec/tile_bag_spec.rb
+++ spec/tile_bag_spec.rb
@@ -12,6 +12,19 @@ describe Scrabble::TileBag do
     it "last letter in the array is Z" do
       expect(@tile_bag.tile_bag.last).to eq("z")
     end
-    
+
+  describe '#draw_tiles(num)' do
+   it 'only accepts numbers' do
+     expect{@tile_bag.draw_tiles("f").to raise_error}
+   end
+   it 'returns num tiles' do
+     tiles = @tile_bag.draw_tiles(4)
+     expect(tiles.length).to eq(4)
+   end
+   it 'removes the tiles from tile_bag_array' do
+     initial_length = @tile_bag.length
+     @tile_bag.draw_tiles(1)
+     expect(@tile_bag.length).to eq(initial_length - 1)
+   end
   end
 end
