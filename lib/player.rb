1233d8687854314f42265e0a3ef42596ede06f86
Riley Spicer <riley.r.spicer@gmail.com>
2015-10-16 11:29:54 -0700

    Matching files, thanks to slack, no thanks to git

--- lib/player.rb
+++ lib/player.rb
@@ -3,56 +3,57 @@ require "tile_bag.rb"
 
 module Scrabble
 
-  class Player
-    attr_accessor :name, :score, :plays, :score_array, :tiles_array, :tiles
+ class Player
+   attr_accessor :name, :score, :plays, :score_array, :tiles_array, :tiles
 
 
-    def initialize(name)
-      @name = name
-      @score = 0
-      @plays = []
-      @score_array = []
-      @tiles= []
-    end
+   def initialize(name)
+     @name = name
+     @score = 0
+     @plays = []
+     @score_array = []
+     @tiles= []
+     @tile_bag = TileBag.new
+   end
 
 
-    def play(word)
-      if won?
-        return false
-      else
-        @plays.push(word)
-      end
-    end
+   def play(word)
+     if won?
+       return false
+     else
+       @plays.push(word)
+     end
+   end
 
-    def total_score
-      @score = 0
-      @plays.each do |word|
-        @score += Scrabble.score(word)
-      end
-      return @score
-    end
+   def total_score
+     @score = 0
+     @plays.each do |word|
+       @score += Scrabble.score(word)
+     end
+     return @score
+   end
 
-    def won?
-      if @score > 100
-        return true
-      else
-        return false
-      end
-    end
+   def won?
+     if @score > 100
+       return true
+     else
+       return false
+     end
+   end
 
-    def highest_scoring_word
-      return Scrabble.highest_score_from(@plays)
-    end
+   def highest_scoring_word
+     return Scrabble.highest_score_from(@plays)
+   end
 
-    def highest_word_score
-      return Scrabble.score(highest_scoring_word)
-    end
+   def highest_word_score
+     return Scrabble.score(highest_scoring_word)
+   end
 
-    def draw_tiles(tile_bag)
-      raise ArgumentError if (tile_bag.length > 7)
-      num_tiles = 7 - tile_bag.length
-      tile_bag.push(@tile_bag.draw_tiles(num_tiles))
-      return tile_bag.flatten!
-    end
-  end
+   def draw_tiles(tile_bag)
+     raise ArgumentError if (tile_bag.length > 7)
+     num_tiles = 7 - tile_bag.length
+     tile_bag.push(@tile_bag.draw_tiles(num_tiles))
+     return tile_bag.flatten!
+   end
+ end
 end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -1,89 +1,89 @@
 require "./lib/player.rb"
 
 describe Scrabble::Player do
-  before :each do
-    @player = Scrabble::Player.new("Ada")
-  end
+ before :each do
+   @player = Scrabble::Player.new("Ada")
+ end
 
-  describe "#initialize" do
-  it "has a name" do
-    expect(@player.name).to eq("Ada")
-    end
-  end
+ describe "#initialize" do
+ it "has a name" do
+   expect(@player.name).to eq("Ada")
+   end
+ end
 
-  describe "plays" do
-    it "return the words array" do
-    expect(@player.plays).to eq([])
-    end
-  end
+ describe "plays" do
+   it "return the words array" do
+   expect(@player.plays).to eq([])
+   end
+ end
 
-  describe "play" do
-    it "Adds the input word to the plays Array" do
-      @player.play("apple")
-      expect(@player.plays).to eq(["apple"])
-    end
-    it "return an empty array when there are no words" do
-      @player.play("")
-      expect(@player.plays  ).to eq ([""])
-    end
-    it "Returns false if player has already won" do
-      @player.score = 101
-      expect(@player.play("apple")).to eq(false)
-    end
-  end
+ describe "play" do
+   it "Adds the input word to the plays Array" do
+     @player.play("apple")
+     expect(@player.plays).to eq(["apple"])
+   end
+   it "return an empty array when there are no words" do
+     @player.play("")
+     expect(@player.plays  ).to eq ([""])
+   end
+   it "Returns false if player has already won" do
+     @player.score = 101
+     expect(@player.play("apple")).to eq(false)
+   end
+ end
 
-  describe "#total_score" do
-    it  "sums up and returns scores" do
-      @player.plays = ["aaa", "b", "c"]
-      @player.total_score
-      expect(@player.score).to eq(9)
-    end
-  end
+ describe "#total_score" do
+   it  "sums up and returns scores" do
+     @player.plays = ["aaa", "b", "c"]
+     @player.total_score
+     expect(@player.score).to eq(9)
+   end
+ end
 
-  describe "#won?" do
-    it "player has > 100 points" do
-      @player.score = 300
-      expect(@player.won?).to eq(true)
-    end
-    it "player has < 100 points" do
-      @player.score = 50
-      expect(@player.won?).to eq(false)
-    end
-  end
+ describe "#won?" do
+   it "player has > 100 points" do
+     @player.score = 300
+     expect(@player.won?).to eq(true)
+   end
+   it "player has < 100 points" do
+     @player.score = 50
+     expect(@player.won?).to eq(false)
+   end
+ end
 
-  describe "#highest_scoring_word" do
-    it "returns highest scoring word" do
-      @player.plays = ["aaa", "bbb", "zzz"]
-      expect(@player.highest_scoring_word).to eq("zzz")
-    end
-    it 'returns a string' do
-      @player.plays = ["aaa", "bbb", "zzz"]
-      expect(@player.highest_scoring_word.class).to eq(String)
-    end
-  end
+ describe "#highest_scoring_word" do
+   it "returns highest scoring word" do
+     @player.plays = ["aaa", "bbb", "zzz"]
+     expect(@player.highest_scoring_word).to eq("zzz")
+   end
+   it 'returns a string' do
+     @player.plays = ["aaa", "bbb", "zzz"]
+     expect(@player.highest_scoring_word.class).to eq(String)
+   end
+ end
 
-  describe "#highest_word_score" do
-    it "returns the score of the highest scoring word" do
-      @player.plays = ["aaa", "bbb", "zzz"]
-      expect(@player.highest_word_score).to eq(30)
-    end
-  end
+ describe "#highest_word_score" do
+   it "returns the score of the highest scoring word" do
+     @player.plays = ["aaa", "bbb", "zzz"]
+     expect(@player.highest_word_score).to eq(30)
+   end
+ end
 
-  describe "tiles" do
-    it 'has no words in the beginning of the game' do
-      expect(@player.tiles).to eq([])
-    end
-    it 'It can\'t contain more then 7 words' do
-      array = ["a","b","c","r","g","y","g","h"]
-      expect{@player.draw_tiles(array)}.to raise_error(ArgumentError)
-    end
-  end
+ describe "tiles" do
+   it 'has no words in the beginning of the game' do
+     expect(@player.tiles).to eq([])
+   end
+   it 'It can\'t contain more then 7 words' do
+     array = ["a","b","c","r","g","y","g","h"]
+     expect{@player.draw_tiles(array)}.to raise_error(ArgumentError)
+   end
+ end
 
-  describe "draw_tiles" do
-    it 'fills tiles array until it has 7 letters from the given tile bag' do
-      check_tile_bag = @player.draw_tiles(["a","b"])
-      expect(check_tile_bag).to eq (6)
-    end
-  end
+ describe "draw_tiles" do
+   it 'fills tiles array until it has 7 letters from the given tile bag' do
+     check_tile_bag = @player.draw_tiles(["a","b"])
+     expect(check_tile_bag.length).to eq (7)
+   end
+ end
 
 end
